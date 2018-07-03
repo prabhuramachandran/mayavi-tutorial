2d7e5fcd14e926648d1ab13ec96b23bc669bddb1
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2018-07-02 23:57:58 -0400

    Add first cut of introduction slides.

---
+++ slides/dodo.py
@@ -0,0 +1,20 @@
+import glob
+import os
+
+
+DOIT_CONFIG = {'default_tasks': ['notebook']}
+
+
+def task_notebook():
+    for fname in glob.glob('*.ipyml'):
+        name = os.path.splitext(fname)[0]
+        dest = name + '.ipynb'
+        yield {
+            'name': name,
+            'actions': [
+                'ipyaml --no-output {src} {dest}'.format(src=fname, dest=dest)
+            ],
+            'file_dep': ['common.tex', fname],
+            'targets': [dest],
+            'clean': True,
+        }
